2113ca09790cbb59c82920837663a94417f5ca75
mcquenji <benjamin.mceachnie@necode.it>
2025-01-13 01:22:34 +0000

    fix: remove unused module variable in post_gen.dart

--- module/hooks/post_gen.dart
+++ module/hooks/post_gen.dart
@@ -3,7 +3,6 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) {
   final name = (context.vars['name'] as String).snakeCase;
-  final module = (context.vars['module'] as String).snakeCase;
   final package = (context.vars['package'] as String).snakeCase;
 
   final f = File('lib/$package.dart');
